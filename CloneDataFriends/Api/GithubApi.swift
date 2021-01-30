56951c1cd88f3fb8295db5271abb87a10c68432c
LeeGiTaek <coolfamily77@gmail.com>
2021-01-31 02:34:34 +0900

    add github api class in new file

---
+++ CloneDataFriends/Api/GithubApi.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApi.swift
+//  CloneDataFriends
+//
+//  Created by gitaeklee on 2021/01/31.
+//
+
+import Foundation
+
+final class GitHubApi {}
+
+// MARK: -API struct & extension
+extension GitHubApi {
+
+  
+    
+}
