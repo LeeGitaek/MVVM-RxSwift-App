9169a508abb4c893313a8d64d5c314ef276414ba
LeeGiTaek <coolfamily77@gmail.com>
2021-01-31 01:58:34 +0900

    Initial Commit

---
+++ CloneDataFriends/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CloneDataFriends
+//
+//  Created by gitaeklee on 2021/01/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
