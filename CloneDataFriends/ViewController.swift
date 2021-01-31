b19c33871f626bc87650f7d33d27119e333cea82
LeeGiTaek <coolfamily77@gmail.com>
2021-01-31 15:36:21 +0900

    modified => storyboard for tableview and viewcontroller code

--- CloneDataFriends/ViewController.swift
+++ CloneDataFriends/ViewController.swift
@@ -6,15 +6,37 @@
 //
 
 import UIKit
-
+import RxCocoa
+import RxSwift
 
 class ViewController: UIViewController {
 
+    static func make(with viewModel:MainViewModel) -> ViewController {
+        let view = ViewController.instantiate()
+        view.viewModel = viewModel
+        return view
+    }
+    
+    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var indicatorView: UIActivityIndicatorView!
+
+    private var viewModel: MainViewModelType!
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        self.bind()
         // Do any additional setup after loading the view.
+    }
+    
+    func bind()
+    {
+        
     }
 
 
 }
 
+extension ViewController: StoryboardInstantiable {}
